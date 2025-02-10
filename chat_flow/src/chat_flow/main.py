5ed6f207b7e15ee3161ee7539ff6cfccf8606291
etsai7 <e.tsai70@gmail.com>
2025-02-10 10:05:48 -0500

    feat: delineating agent and user in chat

--- chat_flow/src/chat_flow/main.py
+++ chat_flow/src/chat_flow/main.py
@@ -17,20 +17,21 @@ class ChatState(BaseModel):
 class ChatFlow(Flow[ChatState]):
     @start()
     def get_business_name(self):
-        self.state.business_name = input("Welcome to Business Business. Please enter your business name: ")
+        print("Agent: Welcome to Business Business Agent. Please enter your business name: ")
+        self.state.business_name = input("User: ")
         return self.state.business_name
 
     @listen(get_business_name)
     def get_website_url(self, business_name):
-        print(f'{business_name} is one of the business names of all times')
-        url = self.state.website_url = input("Please enter your website url: ")
+        print(f'Agent: {business_name} is one of the business names of all times.\n\t\tPlease enter your website url:')
+        url = self.state.website_url = input("User: ")
         results = WebsiteCrew().crew().kickoff(inputs = {"website_url": url})
         self.state.business_description = results
 
     @listen(get_website_url)
     def get_naics_industry(self):
         results = IndustryCrew().crew().kickoff(inputs = {"business_description": str(self.state.business_description)})
-        print(f"Great! This is what we found about your website:\n{results}")
+        print(f"Agent Great! This is what we found about your website:\n{results}")
         # print()
         # print(self.state.model_dump_json(indent=4))
 
